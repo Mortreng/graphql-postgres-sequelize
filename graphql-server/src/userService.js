57cc652a792a181d60352df748777fa82291c057
Danil <Abgarov@mail.ru>
2020-08-17 18:18:40 +0300

    Moved functions from "resolvers" to "userService"

--- graphql-server/src/userService.js
+++ graphql-server/src/userService.js
@@ -1,10 +1,49 @@
 import { User } from "./models/user.model.js";
 import { Op, where } from "sequelize";
 
-let UserService = {
-    findUserById: function (id) {
-        return User.findByPk(id)
-    }
+export function getUserById(id) {
+    return User.findOne({
+        where:{
+            id: {
+                [Op.eq]:id
+            }
+        }
+    });
+}
 
+export async function updateUser(id,name,email,age){
+    let user = getUserById(id)
+    let newUser = await User.update({
 
+        name: name ? name :user.name,
+        email: email ? email : user.email,
+        age: age ? age : user.age
+    }, {
+        where:{
+            id:{
+                [Op.eq]:id
+            }
+        }    
+    });
+    return getUserById(id)
+}
+
+export async function createUser(name,email,age){
+    let freshUser = await User.create({
+        name, email, age
+    });
+    return freshUser
+}
+
+export async function deleteUser(id){
+    let deletedUser = await User.destroy({
+        where : {
+            id: {
+                [Op.eq]:id
+            }
+        }
+    });
+    if (deleteUser<1)
+        throw Error('No user with this id${id}')
+    return true;
 }
